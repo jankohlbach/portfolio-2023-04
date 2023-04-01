ab3f962022d5456b922c36afa3ea932fa5a35eeb
Jan Kohlbach <jan@dorfjungs.com>
2023-04-01 22:23:22 +0200

    🔧 add storyblok connection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,9 @@ export default defineNuxtConfig({
       ]
     }
   },
+  modules: [
+    ['@storyblok/nuxt', { accessToken: process.env.STORYBLOK_ACCESS_TOKEN }]
+  ],
   typescript: {
     typeCheck: true
   },
