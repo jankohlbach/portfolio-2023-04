055e261de9842de07cf1f130bbfd82fa648de3c2
Jan Kohlbach <jan@dorfjungs.com>
2023-05-01 16:02:04 +0200

    ✨ modules > add awards module

---
+++ storyblok/ModuleAwards.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+defineProps({
+  blok: {
+    type: Object,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <section class="module-awards">
+    <div class="temp-watermark">
+      www
+    </div>
+    <div class="wrap">
+      <h2>{{ blok.text_pre }}</h2>
+
+      <div class="awards">
+        <h3 v-for="award in blok.blocks_awards" :key="award._uid" class="award">
+          {{ award.text }}
+        </h3>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '~/assets/styles/functions' as *;
+
+section {
+  position: relative;
+  display: flex;
+  width: 100%;
+  min-height: 100vh;
+  margin-top: rem(320);
+  background-color: var(--c-dark);
+  color: var(--c-light);
+}
+
+.temp-watermark {
+  position: absolute;
+  inset: 0;
+  margin: auto;
+  text-align: center;
+  line-height: 100vh;
+  font-size: 30vmin;
+  opacity: 0.2;
+}
+
+.wrap {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100%;
+  padding-block: rem(100);
+  margin-inline: var(--s-s);
+}
+
+h2 {
+  font-size: var(--fs-base);
+  font-weight: 200;
+}
+
+h3 {
+  font-size: var(--fs-m);
+  font-weight: 100;
+  line-height: 1.5;
+}
+</style>
