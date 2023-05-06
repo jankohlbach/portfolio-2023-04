3de69ddb7313b773f98f7b9ca6599b89230226cd
Jan Kohlbach <jan@dorfjungs.com>
2023-05-06 15:22:35 +0200

    ✨ modules > add editorial module

---
+++ storyblok/ModuleEditorial.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps({
+  blok: {
+    type: Object,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <section class="module-editorial">
+    <img v-for="block in blok.blocks_images" :key="block._uid" :src="block.image.filename" :alt="block.image.alt">
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  margin-inline: var(--s-s);
+  margin-top: rem(320);
+}
+
+img {
+  max-width: 100%;
+  object-fit: cover;
+
+  + img {
+    margin-top: rem(100);
+  }
+
+  &:nth-child(odd) {
+    width: 690px;
+    aspect-ratio: 690 / 690;
+  }
+
+  &:nth-child(even) {
+    margin-left: auto;
+    margin-right: rem(100);
+    width: 690px;
+    aspect-ratio: 690 / 920;
+  }
+}
+</style>
