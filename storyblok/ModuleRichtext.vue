74c6bec7603b35219827786199b542df3c60c1ec
Jan Kohlbach <jan@dorfjungs.com>
2023-05-07 17:12:30 +0200

    ✨ modules > add richtext module

---
+++ storyblok/ModuleRichtext.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps({
+  blok: {
+    type: Object,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <section class="module-richtext">
+    <div v-html="renderRichText(blok.richtext_text)" />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  margin-inline: var(--s-s);
+  margin-block: rem(320);
+}
+
+div {
+  max-width: min(rem(900), 90%);
+
+  @media (min-width: 768px) {
+    margin: auto;
+  }
+}
+</style>
