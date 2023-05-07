84f48c25ab1269231d3d569aa226c55eedb83f73
Jan Kohlbach <jan@dorfjungs.com>
2023-05-07 20:51:48 +0200

    ✨ modules > add contact module

---
+++ storyblok/ModuleContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="module-contact">
+    <div class="wrap">
+      <h2>Want to work on something together, have a request or just wanna say hi?</h2>
+      <a href="mailto:hi@jankohlbach.com">hi@jankohlbach.com</a>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  margin-inline: var(--s-s);
+  margin-top: rem(180);
+}
+
+.wrap {
+  max-width: min(rem(900), 90%);
+
+  @media (min-width: 768px) {
+    margin: auto;
+  }
+}
+
+h2 {
+  font-size: var(--fs-m);
+  font-weight: 100;
+}
+
+a {
+  display: inline-block;
+  margin-top: rem(20);
+  font-size: var(--fs-m);
+}
+</style>
