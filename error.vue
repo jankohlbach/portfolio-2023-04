792603fa340508afff250c6d775178584a29b17f
Jan Kohlbach <jan@dorfjungs.com>
2023-05-07 16:56:58 +0200

    ✨ add basic error page

---
+++ error.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineProps({
+  error: {
+    type: Object,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <div class="layout">
+    <Noise />
+    <Menu />
+    <Header />
+
+    <section>
+      <h1>{{ error.statusCode }}</h1>
+      <h2>{{ error.message }}</h2>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+section {
+  margin-inline: var(--s-s);
+  margin-top: rem(180);
+}
+
+h1 {
+  font-size: var(--fs-xl);
+  font-weight: 100;
+}
+
+h2 {
+  font-size: var(--fs-m);
+  font-weight: 100;
+}
+</style>
