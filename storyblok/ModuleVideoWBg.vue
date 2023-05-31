e06427df7d686f82fe4202f28897a9344e5874f2
Jan Kohlbach <jan@dorfjungs.com>
2023-05-31 16:56:53 +0200

    ✨ modules > add video with background module

---
+++ storyblok/ModuleVideoWBg.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps({
+  blok: {
+    type: Object,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <section class="module-video-w-bg">
+    <img :src="blok.asset_image.filename" :alt="blok.asset_image.alt">
+    <video :src="blok.asset_video.filename" muted autoplay loop playsinline />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  margin-top: rem(320);
+  display: grid;
+}
+
+img,
+video {
+  grid-area: 1 / 1;
+}
+
+video {
+  width: 75%;
+  margin: auto;
+}
+</style>
